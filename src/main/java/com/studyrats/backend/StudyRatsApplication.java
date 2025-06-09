4a081d69a28f88e3171d48aa45d86c9342ffe465
raidejunior <raidejunior@gmail.com>
2025-06-09 13:27:25 -0300

    feat: Aplicação inicial

---
+++ src/main/java/com/studyrats/backend/StudyRatsApplication.java
@@ -0,0 +1,13 @@
+package com.studyrats.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyrats/backend/StudyRatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyrats.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
